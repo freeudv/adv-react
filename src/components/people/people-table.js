7db5558f222e852214d0e37a8101a663539e5b4d
Dmitry Umbras <freeudv@mail.ru>
2018-03-13 11:02:43 +0300

    lesson 5 - add animated people add in people table

--- src/components/people/people-table.js
+++ src/components/people/people-table.js
@@ -2,30 +2,63 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import { peopleListSelector, fetchAllPeople } from "../../ducks/people"
 import { Table, Column } from "react-virtualized"
+import { TransitionMotion, spring } from 'react-motion'
 
 export class PeopleTable extends Component {
-  rowGetter = ({ index }) => this.props.people[index]
-
   componentDidMount() {
     this.props.fetchAllPeople && this.props.fetchAllPeople()
   }
 
+  componentDidUpdate({ people }) {
+    if (people.length && this.props.people.length > people.length) {
+      setTimeout(() => {
+        this.table.scrollToRow(this.props.people.length)
+      }, 0)
+    }
+  }
+
+  rowGetter = ({ index }) => this.props.people[index]
+
+  willEnter = () => ({
+    opacity: 0
+  })
+
+  getStyles = () => this.props.people.map(person => ({
+    key: person.uid,
+    style: {
+      opacity: spring(1)
+    },
+    data: person
+  }))
+
+  setListRef = ref => this.table = ref
+
   render() {
     return (
-      <Table
-        width={600}
-        height={300}
-        rowHeight={40}
-        headerHeight={50}
-        rowGetter={this.rowGetter}
-        rowCount={this.props.people.length}
-        overscanRowCount={2}
-        rowClassName="test--people-list__row"
+      <TransitionMotion
+        willEnter={this.willEnter}
+        styles={this.getStyles}
       >
-        <Column label="First Name" dataKey="firstName" width={200} />
-        <Column label="Last Name" dataKey="lastName" width={200} />
-        <Column label="Email" dataKey="email" width={200} />
-      </Table>
+        {
+          interpolatedStyles =>
+            <Table
+              width={600}
+              height={300}
+              rowHeight={40}
+              headerHeight={50}
+              rowGetter={this.rowGetter}
+              rowCount={interpolatedStyles.length}
+              overscanRowCount={2}
+              rowClassName="test--people-list__row"
+              rowStyle={({ index }) => index < 0 ? {} : interpolatedStyles[index].style}
+              ref={this.setListRef}
+            >
+              <Column label="First Name" dataKey="firstName" width={200} />
+              <Column label="Last Name" dataKey="lastName" width={200} />
+              <Column label="Email" dataKey="email" width={200} />
+            </Table>
+        }
+      </TransitionMotion>
     )
   }
 }
